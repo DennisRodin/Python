afd95eca40f223f7898c64c9f719c4e3430242cf
DennisRodin <55505799+DennisRodin@users.noreply.github.com>
2019-10-10 00:06:36 +0200

    Add files via upload

---
+++ PigLatin.py
@@ -0,0 +1,15 @@
+#This is a Pig Latin translator.
+
+print ("Welcome to the Pig Latin Translator!")
+
+pyg = "ay"
+original = input("Enter a word:")
+if len(original) > 0 and original.isalpha():
+    word = original.lower()
+    first = word[0]
+    new_word = word + first + pyg
+    new_word = new_word[1:len(new_word)]
+else:
+    print("empty")
+
+print(new_word)

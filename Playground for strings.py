1b2ffca5c9d7b2e4f1214a0eac5afea21fb0d6d4
DennisRodin <55505799+DennisRodin@users.noreply.github.com>
2019-10-09 22:09:17 +0200

    Add files via upload

--- Playground for strings.py
+++ Playground for strings.py
@@ -7,10 +7,12 @@ uppered_text = "----This text fragment was uppered via a code. ----"
 
 lowered_text = "----This text fragment was lowered via a code. ----" 
 
-strip_text   = "----This text contains eight hyphens.----" 
+strip_text   = "----This text contains eight hyphens. ----" 
+
+replaced_text= "----In this text all the 'T' have been replaced with the 'X'. ----"
+
+capitalized_t= "---- This text just got a capitalized 'T'. ---- "
 
 
 
-
-
-print(str(base_text + uppered_text.upper() + lowered_text.lower() + strip_text.strip("-") ))
+print(str(base_text + uppered_text.upper() + lowered_text.lower() + strip_text.strip("-") + replaced_text.replace("t", "x") + capitalized_t.capitalize() ))
